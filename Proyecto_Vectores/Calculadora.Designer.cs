0f18a4090ff94c6f0e0e01b0a71b51c923369d55
Clay Arizala <clay.arizala@pucese.edu.ec>
2020-08-28 18:45:40 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Vectores/Calculadora.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Proyecto_Vectores
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPantalla = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btbigual = new System.Windows.Forms.Button();
+            this.btbdivision = new System.Windows.Forms.Button();
+            this.btbresta = new System.Windows.Forms.Button();
+            this.btbsuma = new System.Windows.Forms.Button();
+            this.btbmultiplicacion = new System.Windows.Forms.Button();
+            this.btbac = new System.Windows.Forms.Button();
+            this.btb9 = new System.Windows.Forms.Button();
+            this.btb8 = new System.Windows.Forms.Button();
+            this.btb7 = new System.Windows.Forms.Button();
+            this.btb6 = new System.Windows.Forms.Button();
+            this.btb5 = new System.Windows.Forms.Button();
+            this.btb4 = new System.Windows.Forms.Button();
+            this.btb3 = new System.Windows.Forms.Button();
+            this.btb2 = new System.Windows.Forms.Button();
+            this.btb1 = new System.Windows.Forms.Button();
+            this.btb0 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPantalla
+            // 
+            this.txtPantalla.Location = new System.Drawing.Point(45, 54);
+            this.txtPantalla.Name = "txtPantalla";
+            this.txtPantalla.Size = new System.Drawing.Size(335, 20);
+            this.txtPantalla.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btbigual);
+            this.groupBox1.Controls.Add(this.btbdivision);
+            this.groupBox1.Controls.Add(this.btbresta);
+            this.groupBox1.Controls.Add(this.btbsuma);
+            this.groupBox1.Controls.Add(this.btbmultiplicacion);
+            this.groupBox1.Controls.Add(this.btbac);
+            this.groupBox1.Controls.Add(this.btb9);
+            this.groupBox1.Controls.Add(this.btb8);
+            this.groupBox1.Controls.Add(this.btb7);
+            this.groupBox1.Controls.Add(this.btb6);
+            this.groupBox1.Controls.Add(this.btb5);
+            this.groupBox1.Controls.Add(this.btb4);
+            this.groupBox1.Controls.Add(this.btb3);
+            this.groupBox1.Controls.Add(this.btb2);
+            this.groupBox1.Controls.Add(this.btb1);
+            this.groupBox1.Controls.Add(this.btb0);
+            this.groupBox1.Location = new System.Drawing.Point(45, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 192);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // btbigual
+            // 
+            this.btbigual.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbigual.Location = new System.Drawing.Point(270, 154);
+            this.btbigual.Margin = new System.Windows.Forms.Padding(2);
+            this.btbigual.Name = "btbigual";
+            this.btbigual.Size = new System.Drawing.Size(56, 26);
+            this.btbigual.TabIndex = 33;
+            this.btbigual.Text = "=";
+            this.btbigual.UseVisualStyleBackColor = true;
+            // 
+            // btbdivision
+            // 
+            this.btbdivision.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbdivision.Location = new System.Drawing.Point(270, 109);
+            this.btbdivision.Margin = new System.Windows.Forms.Padding(2);
+            this.btbdivision.Name = "btbdivision";
+            this.btbdivision.Size = new System.Drawing.Size(56, 26);
+            this.btbdivision.TabIndex = 32;
+            this.btbdivision.Text = "/";
+            this.btbdivision.UseVisualStyleBackColor = true;
+            // 
+            // btbresta
+            // 
+            this.btbresta.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbresta.Location = new System.Drawing.Point(270, 59);
+            this.btbresta.Margin = new System.Windows.Forms.Padding(2);
+            this.btbresta.Name = "btbresta";
+            this.btbresta.Size = new System.Drawing.Size(56, 26);
+            this.btbresta.TabIndex = 31;
+            this.btbresta.Text = "-";
+            this.btbresta.UseVisualStyleBackColor = true;
+            // 
+            // btbsuma
+            // 
+            this.btbsuma.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbsuma.Location = new System.Drawing.Point(270, 13);
+            this.btbsuma.Margin = new System.Windows.Forms.Padding(2);
+            this.btbsuma.Name = "btbsuma";
+            this.btbsuma.Size = new System.Drawing.Size(56, 26);
+            this.btbsuma.TabIndex = 30;
+            this.btbsuma.Text = "+";
+            this.btbsuma.UseVisualStyleBackColor = true;
+            // 
+            // btbmultiplicacion
+            // 
+            this.btbmultiplicacion.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbmultiplicacion.Location = new System.Drawing.Point(195, 150);
+            this.btbmultiplicacion.Margin = new System.Windows.Forms.Padding(2);
+            this.btbmultiplicacion.Name = "btbmultiplicacion";
+            this.btbmultiplicacion.Size = new System.Drawing.Size(56, 26);
+            this.btbmultiplicacion.TabIndex = 29;
+            this.btbmultiplicacion.Text = "X";
+            this.btbmultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // btbac
+            // 
+            this.btbac.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btbac.Location = new System.Drawing.Point(100, 150);
+            this.btbac.Margin = new System.Windows.Forms.Padding(2);
+            this.btbac.Name = "btbac";
+            this.btbac.Size = new System.Drawing.Size(56, 26);
+            this.btbac.TabIndex = 28;
+            this.btbac.Text = "AC";
+            this.btbac.UseVisualStyleBackColor = true;
+            // 
+            // btb9
+            // 
+            this.btb9.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb9.Location = new System.Drawing.Point(195, 13);
+            this.btb9.Margin = new System.Windows.Forms.Padding(2);
+            this.btb9.Name = "btb9";
+            this.btb9.Size = new System.Drawing.Size(56, 26);
+            this.btb9.TabIndex = 27;
+            this.btb9.Text = "9";
+            this.btb9.UseVisualStyleBackColor = true;
+            // 
+            // btb8
+            // 
+            this.btb8.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb8.Location = new System.Drawing.Point(100, 13);
+            this.btb8.Margin = new System.Windows.Forms.Padding(2);
+            this.btb8.Name = "btb8";
+            this.btb8.Size = new System.Drawing.Size(56, 26);
+            this.btb8.TabIndex = 26;
+            this.btb8.Text = "8";
+            this.btb8.UseVisualStyleBackColor = true;
+            // 
+            // btb7
+            // 
+            this.btb7.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb7.Location = new System.Drawing.Point(9, 13);
+            this.btb7.Margin = new System.Windows.Forms.Padding(2);
+            this.btb7.Name = "btb7";
+            this.btb7.Size = new System.Drawing.Size(56, 26);
+            this.btb7.TabIndex = 25;
+            this.btb7.Text = "7";
+            this.btb7.UseVisualStyleBackColor = true;
+            // 
+            // btb6
+            // 
+            this.btb6.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb6.Location = new System.Drawing.Point(195, 59);
+            this.btb6.Margin = new System.Windows.Forms.Padding(2);
+            this.btb6.Name = "btb6";
+            this.btb6.Size = new System.Drawing.Size(56, 26);
+            this.btb6.TabIndex = 24;
+            this.btb6.Text = "6";
+            this.btb6.UseVisualStyleBackColor = true;
+            // 
+            // btb5
+            // 
+            this.btb5.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb5.Location = new System.Drawing.Point(100, 59);
+            this.btb5.Margin = new System.Windows.Forms.Padding(2);
+            this.btb5.Name = "btb5";
+            this.btb5.Size = new System.Drawing.Size(56, 26);
+            this.btb5.TabIndex = 23;
+            this.btb5.Text = "5";
+            this.btb5.UseVisualStyleBackColor = true;
+            // 
+            // btb4
+            // 
+            this.btb4.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb4.Location = new System.Drawing.Point(9, 59);
+            this.btb4.Margin = new System.Windows.Forms.Padding(2);
+            this.btb4.Name = "btb4";
+            this.btb4.Size = new System.Drawing.Size(56, 26);
+            this.btb4.TabIndex = 22;
+            this.btb4.Text = "4";
+            this.btb4.UseVisualStyleBackColor = true;
+            // 
+            // btb3
+            // 
+            this.btb3.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb3.Location = new System.Drawing.Point(195, 109);
+            this.btb3.Margin = new System.Windows.Forms.Padding(2);
+            this.btb3.Name = "btb3";
+            this.btb3.Size = new System.Drawing.Size(56, 26);
+            this.btb3.TabIndex = 21;
+            this.btb3.Text = "3";
+            this.btb3.UseVisualStyleBackColor = true;
+            // 
+            // btb2
+            // 
+            this.btb2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb2.Location = new System.Drawing.Point(100, 109);
+            this.btb2.Margin = new System.Windows.Forms.Padding(2);
+            this.btb2.Name = "btb2";
+            this.btb2.Size = new System.Drawing.Size(56, 26);
+            this.btb2.TabIndex = 20;
+            this.btb2.Text = "2";
+            this.btb2.UseVisualStyleBackColor = true;
+            // 
+            // btb1
+            // 
+            this.btb1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb1.Location = new System.Drawing.Point(9, 109);
+            this.btb1.Margin = new System.Windows.Forms.Padding(2);
+            this.btb1.Name = "btb1";
+            this.btb1.Size = new System.Drawing.Size(56, 26);
+            this.btb1.TabIndex = 19;
+            this.btb1.Text = "1";
+            this.btb1.UseVisualStyleBackColor = true;
+            // 
+            // btb0
+            // 
+            this.btb0.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb0.Location = new System.Drawing.Point(9, 150);
+            this.btb0.Margin = new System.Windows.Forms.Padding(2);
+            this.btb0.Name = "btb0";
+            this.btb0.Size = new System.Drawing.Size(56, 26);
+            this.btb0.TabIndex = 18;
+            this.btb0.Text = "0";
+            this.btb0.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 322);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPantalla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPantalla;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btbigual;
+        private System.Windows.Forms.Button btbdivision;
+        private System.Windows.Forms.Button btbresta;
+        private System.Windows.Forms.Button btbsuma;
+        private System.Windows.Forms.Button btbmultiplicacion;
+        private System.Windows.Forms.Button btbac;
+        private System.Windows.Forms.Button btb9;
+        private System.Windows.Forms.Button btb8;
+        private System.Windows.Forms.Button btb7;
+        private System.Windows.Forms.Button btb6;
+        private System.Windows.Forms.Button btb5;
+        private System.Windows.Forms.Button btb4;
+        private System.Windows.Forms.Button btb3;
+        private System.Windows.Forms.Button btb2;
+        private System.Windows.Forms.Button btb1;
+        private System.Windows.Forms.Button btb0;
+    }
+}
+
---
+++ Proyecto_Vectores/Calculadora.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Vectores
+{
+    public enum TipoTecla
+    {
+        NINGUNO = 0,
+        DIGITO = 1,
+        OPERADOR = 2,
+        AC = 3
+    }
+    public partial class Calculadora : Form
+    {
+        private TipoTecla mUltimaPulsacion;
+        private int mOperando1;
+        private int mOperando2;
+
+        private int mNumOperandos;
+        private string mOperador;
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+        bool validaDigito(string teclapulsada)
+        {
+            if (teclapulsada.Equals("0")) return true;
+            if (teclapulsada.Equals("1")) return true;
+            if (teclapulsada.Equals("2")) return true;
+            if (teclapulsada.Equals("3")) return true;
+            if (teclapulsada.Equals("4")) return true;
+            if (teclapulsada.Equals("5")) return true;
+            if (teclapulsada.Equals("6")) return true;
+            if (teclapulsada.Equals("7")) return true;
+            if (teclapulsada.Equals("8")) return true;
+            if (teclapulsada.Equals("9")) return true;
+            return false;
+        }
+        private void btb0_Click(object sender, EventArgs e)
+        {
+            //detectar que botón se pulsó
+            // cast -- casting -- casteo
+            Button btn = (Button)sender;
+            string strnum = btn.Text;
+
+
+
+            if (this.mUltimaPulsacion != TipoTecla.DIGITO)
+                this.txtPantalla.Text = "";
+
+
+
+            if (validaDigito(strnum))
+            {
+                this.txtPantalla.Text += strnum; //método abreviado
+                this.mUltimaPulsacion = TipoTecla.DIGITO;
+            }
+            else if ((strnum == "+" || strnum == "-" || strnum == "X" || strnum == "/" || strnum == "=") && this.mUltimaPulsacion == TipoTecla.DIGITO)
+            {
+                if (this.mUltimaPulsacion == TipoTecla.DIGITO)
+                    this.mNumOperandos++;
+
+                if (this.mNumOperandos == 1)
+                {
+                    this.mOperando1 = Int32.Parse(this.txtPantalla.Text);
+                }
+
+                else if (this.mNumOperandos == 2)
+                {
+                    this.mOperando2 = Int32.Parse(this.txtPantalla.Text);
+                    switch (this.mOperador)
+                    {
+                        case "+":
+                            this.mOperando1 += this.mOperando2;
+                            break;
+                        case "-":
+                            this.mOperando1 -= this.mOperando2;
+                            break;
+                        case "X":
+                            this.mOperando1 *= this.mOperando2;
+                            break;
+                        case "/":
+                            this.mOperando1 /= this.mOperando2;
+                            break;
+                        case "=":
+                            this.txtPantalla.Show();
+                            break;
+                    }
+                    this.txtPantalla.Text = this.mOperando1.ToString();
+                    this.mNumOperandos = 1;
+                }
+
+
+
+                //this.txtPantalla.Text = "";
+
+                this.mUltimaPulsacion = TipoTecla.OPERADOR;
+                this.mOperador = strnum;
+            }
+
+            if (strnum == "AC")
+            {
+                this.txtPantalla.Clear();
+                this.mNumOperandos = 0;
+            }
+
+
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+            this.mUltimaPulsacion = TipoTecla.NINGUNO;
+            this.mOperando1 = 0;
+            this.mOperando2 = 0;
+            this.mNumOperandos = 0;
+            this.mUltimaPulsacion = TipoTecla.NINGUNO;
+
+            btb1.Click += new EventHandler(btb0_Click);
+            btb2.Click += new EventHandler(btb0_Click);
+            btb3.Click += new EventHandler(btb0_Click);
+            btb4.Click += new EventHandler(btb0_Click);
+            btb5.Click += new EventHandler(btb0_Click);
+            btb6.Click += new EventHandler(btb0_Click);
+            btb7.Click += new EventHandler(btb0_Click);
+            btb8.Click += new EventHandler(btb0_Click);
+            btb9.Click += new EventHandler(btb0_Click);
+
+            this.btbsuma.Click += new EventHandler(btb0_Click);
+            this.btbresta.Click += new EventHandler(btb0_Click);
+            this.btbmultiplicacion.Click += new EventHandler(btb0_Click);
+            this.btbdivision.Click += new EventHandler(btb0_Click);
+            this.btbigual.Click += new EventHandler(btb0_Click);
+            this.btbac.Click += new EventHandler(btb0_Click);
+        }
+
+        private void btb7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
